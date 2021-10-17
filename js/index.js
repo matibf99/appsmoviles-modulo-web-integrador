c1fae774b316f4e9364415c025d0ed6da66f4962
matibf99 <matibf99@gmail.com>
2021-10-17 02:07:16 -0300

    Nuevo diseño de navbar y footer

--- js/index.js
+++ js/index.js
@@ -19,12 +19,3 @@ const containerHistory = $(".history-content");
 /* Load history */
 
 loadHistory();
-
-//nav toggle -select button and links
-const navToggle = document.querySelector("#navToggle");
-const nav = document.querySelector("#nav-links");
-
-//add event listener
-navToggle.addEventListener("click", () => {
-    nav.classList.toggle('nav-open')
-});
--- js/searchbar.js
+++ js/searchbar.js
@@ -1,7 +1,21 @@
-const searchBar = $('#search-bar');
+const searchBar = $("#search-bar");
+const searchButton = $(".buscar-nav-btn");
+const searchNav = $(".buscar-nav");
+
+const navToggle = $(".nav-toggle");
+const navLinks = $(".nav-links");
 
 searchBar.keydown(function (e){
     if(e.keyCode == 13){
         window.location.href = `/html/search.html?q=${searchBar.val()}`;
     }
-})
+});
+
+searchButton.on("click", (e) => {
+    window.location.href = `/html/search.html?q=${searchBar.val()}`;
+});
+
+navToggle.on("click", () => {
+    navLinks.toggle();
+    searchNav.toggle();
+});
