e73ac5f89b12443e6efa9ad5fec33c3664247ffc
matibf99 <matibf99@gmail.com>
2021-10-16 23:24:47 -0300

    Fix hamburger en navbar

--- js/index.js
+++ js/index.js
@@ -19,3 +19,12 @@ const containerHistory = $(".history-content");
 /* Load history */
 
 loadHistory();
+
+//nav toggle -select button and links
+const navToggle = document.querySelector("#navToggle");
+const nav = document.querySelector("#nav-links");
+
+//add event listener
+navToggle.addEventListener("click", () => {
+    nav.classList.toggle('nav-open')
+});
--- js/main.js
+++ js/main.js
@@ -1,12 +1,3 @@
-//nav toggle -select button and links
-const navToggle = document.querySelector("#navToggle");
-const nav = document.querySelector("#nav-links");
-
-//add event listener
-navToggle.addEventListener("click", () => {
-    nav.classList.toggle('nav-open')
-});
-
 /* Map */ 
 const mymap = L.map('mapid').setView([-34.9225759, -57.9554079], 13);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(mymap);
